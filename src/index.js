6a852b0593f620d4d8a54b5947a6d59a3ef2d63e
mmishra22 <manishjrm16@gmail.com>
2026-06-18 11:33:04 +0530

    Added

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { First } from './firstcomponent';
- 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <First />
+    <App />
   </React.StrictMode>
 );
 
