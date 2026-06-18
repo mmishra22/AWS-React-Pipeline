22f749ad24041ed37cebdcd4947b537c57ad45e7
mmishra22 <manishjrm16@gmail.com>
2026-06-18 11:29:29 +0530

    Added

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { First } from './Firstcomponent';
+import { First } from './firstcomponent';
+ 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
