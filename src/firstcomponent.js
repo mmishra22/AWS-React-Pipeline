ae2edd851d0adcf69ad87b11f38f28b8bffe9e86
mmishra22 <manishjrm16@gmail.com>
2026-06-18 11:16:31 +0530

    Added

--- src/firstcomponent.js
+++ src/firstcomponent.js
@@ -1,4 +1,4 @@
-export const FirstComponent=()=>{
+export const First=()=>{
     return(
         <div>
             <h2>This is a AWS Pipeline</h2>
--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { FirstComponent } from './firstcomponent';
+import { First } from './Firstcomponent';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <FirstComponent />
+    <First />
   </React.StrictMode>
 );
 
